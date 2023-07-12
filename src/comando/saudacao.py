17f563dd9bb91a2548b0d194be1c4af7fa535d37
João Victor <joaovictormguimaraes@gmail.com>
2023-07-11 23:07:31 -0300

    Atualizacao

--- src/comando/saudacao.py
+++ src/comando/saudacao.py
@@ -1,2 +1,6 @@
-def saudacao(nome):
-    return "Olá, " + nome + "!"
+#def saudacao(nome):
+#    return "Olá, " + nome + "!"
+from unidecode import unidecode
+
+def standardize(text):
+    return unidecode(text)
--- src/comando/test_saudacao.py
+++ src/comando/test_saudacao.py
@@ -1,6 +1,13 @@
-from comando import saudacao
+# from src.comando import saudacao
 
-def teste():
-    saudacao("João")
-    saudacao("Maria")
-    saudacao("Ana")
+# print(saudacao("João"))
+# print(saudacao("Maria"))
+# print(saudacao("Ana"))
+
+from comando import stringstd
+
+
+def test_full_name():
+    name = "Kelly Frazão"
+    std_name = stringstd.standardize(name)
+    assert len(std_name.split(' ')) > 1
