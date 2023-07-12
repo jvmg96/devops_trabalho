d6bc662ea38be6d56a5b6a34e79f417c244c2fe7
João Victor <joaovictormguimaraes@gmail.com>
2023-07-11 22:33:15 -0300

    Atualizacao

--- src/comando/test_saudacao.py
+++ src/comando/test_saudacao.py
@@ -1,8 +1,8 @@
 from comando import saudacao
 
-def test_saudacao():
+def teste():
     assert saudacao("João") == "Olá, João!"
     assert saudacao("Maria") == "Olá, Maria!"
     assert saudacao("Ana") == "Olá, Ana!"
 
-test_saudacao()
+teste()
