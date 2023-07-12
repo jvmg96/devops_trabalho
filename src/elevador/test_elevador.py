c0aa7c24987ed4c702f616adfc982d04242ecd1f
João Victor <joaovictormguimaraes@gmail.com>
2023-07-11 22:27:38 -0300

    Atualizacao

--- src/elevador/test_elevador.py
+++ src/elevador/test_elevador.py
@@ -1,6 +1,21 @@
 from elevador import Elevador
 
-def test_full_name():
-    name = "Kelly Frazão"
-    std_name = Elevador.standardize(name)
-    assert len(std_name.split(' ')) > 1
+def test_entrar():
+    elevador = Elevador(7, 0, 8, 0)
+    elevador.entrar()
+    assert elevador.pessoasDentro == 1
+
+def test_sair():
+    elevador = Elevador(7, 0, 8, 1)
+    elevador.sair()
+    assert elevador.pessoasDentro == 0
+
+def test_subir():
+    elevador = Elevador(7, 0, 8, 0)
+    elevador.subir()
+    assert elevador.andarAtual == 1
+
+def test_descer():
+    elevador = Elevador(7, 1, 8, 0)
+    elevador.descer()
+    assert elevador.andarAtual == 0
